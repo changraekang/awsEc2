5346015fe62fdd301df8147e24fe04a26cc6ca40
changrae kang <89755609+changraekang@users.noreply.github.com>
2023-04-07 09:57:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello Bitvelo EC2 Study'))
+app.get('/', (req, res) => res.send('Hello Node EC2 Study'))
 
 app.listen(port, () => console.log('listening on port ' + port));
